58160237ccf07338581b88e45639c95e8913fc18
Kirsten Nicholson <kirstennic6@gmail.com>
2021-02-08 16:14:03 +0000

    Employee id get

--- src/main/java/com/example/codeclan/employeeData/controllers/EmployeeController.java
+++ src/main/java/com/example/codeclan/employeeData/controllers/EmployeeController.java
@@ -4,9 +4,11 @@ import com.example.codeclan.employeeData.models.Employee;
 import com.example.codeclan.employeeData.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EmployeeController {
@@ -18,4 +20,11 @@ public class EmployeeController {
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
+
+    @GetMapping(value = "/employees/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable Long id){
+        return employeeRepository.findById(id);
+    }
+
+
 }
